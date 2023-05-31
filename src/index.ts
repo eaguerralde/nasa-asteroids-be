c98be4f6382d752670788bce9f2d59e9e63e90df
Eduardo Aguerralde <eaguerralde@gmail.com>
2023-05-31 07:29:02 +0100

    Setup cors module

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
 import { router } from './Favourites/controller';
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/favourites', router)
 app.listen(PORT, () => {
